77785c7c9dcaddaba118dfef7c0c18295975e57a
Benjamin Lorentz <benjy.lorentz@gmail.com>
2018-11-14 13:22:58 +0000

    Implémentation de la fonctionnalité d'E

--- src/main/python/readSerial.py
+++ src/main/python/readSerial.py
@@ -1,18 +1,57 @@
 import serial
+from fractions import Fraction
 
 btSerial = serial.Serial("/dev/rfcomm0", baudrate=9600, timeout=0.5)
 
-def parseBluetoothInput( content ) :
-    print content
-    return content
+componentDictionnary = {
+        'PWR': 'POWEROFF',
+        'READSETTINGS':'READSETTINGS',
+        'SHOOT':'SHOOT',
+        'EV' : 'BRACKET_SIZE',
+        'SP' : 'SPEED',
+}
 
-def main() 
+COMMAND_SEPARATOR = '#'
+
+def parseBluetoothInput( content ) :
+    # Get list of commands
+    cmds = content.split(COMMAND_SEPARATOR)
+    print cmds
+    lastCmd = cmds[-1]
+    if(lastCmd == '') :
+        lastCmd = cmds[-2]
+    return lastCmd
+
+
+
+def readEV ( content ) :
+    value = Fraction(int(content.split('EV')[-1]), 3) 
+    print "EV : " + str(value)
+    sendMessage ("*A" + str(value) + "*A")
+    return 
+
+def sendMessage ( content ) : 
+    btSerial.write( content ) 
+    return
+
+def readFile () :
+    etcPasswd = open("/etc/passwd", "r")
+    print etcPasswd.read() 
+
+def dispatch ( command ) :
+    if( command.startswith('EV') ) :
+        readEV( command ) 
+    else : 
+        return 
+
+def main() :
     while True:
         rcv = btSerial.read(512)
-        rcv = parseBluetoothInput(rcv)
         if rcv:
-            btSerial.write("OK!")
-            print(rcv)
+            cmd = parseBluetoothInput(rcv) 
+            print "Last command:" + cmd
+            dispatch (cmd) 
 
 
 main() 
+#readFile()
